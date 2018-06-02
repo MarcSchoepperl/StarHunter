3b4a725309820f647c42a102e5a1012ab71a9678
Marc Schöpperl <marc@schoepperl.com>
2018-06-02 13:22:50 +0200

    added gtk and application

--- Project/src/Main.cpp
+++ Project/src/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "../include/Application.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
   std::cout << "Hello World" << std::endl;
+
+  Application::Application App;
+
+  App.Init(argc, argv);
+  App.Run();
 
   return 0;
 }
